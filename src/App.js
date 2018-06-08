d595856d91a9785cb5801151bfff86890a79edcb
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-08 16:57:40 -0400

    add appbar, forecast extended and grid layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import React, { Component } from "react";
-import "./App.css";
+import { Grid, Col, Row } from "react-flexbox-grid";
 
-import WeatherLocation from "./Components/WeatherLocation/WeatherLocation";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+import LocationList from "./Components/LocationList/LocationList";
+import ForecastExtended from "./Components/ForecastExtended/ForecastExtended";
+
+const cities = [
+  "Buenos aires, ar",
+  "Caracas, ve",
+  "Bogota, co",
+  "Santiago, cl",
+  "Washington",
+  "Barcelona"
+];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: ""
+    };
+  }
+
+  onSelectedLocation = city => {
+    console.log(city);
+    this.setState({ city });
+  };
   render() {
     return (
-      <div className="App">
-        <WeatherLocation />
-      </div>
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <AppBar position="static" color="default" title="hola mundo">
+              <Toolbar>
+                <Typography variant="title" color="inherit">
+                  Title
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList
+              cities={cities}
+              onSelectedLocation={this.onSelectedLocation}
+            />
+          </Col>
+          <Col xs={12} md={6}>
+            <ForecastExtended city={this.state.city} />
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
