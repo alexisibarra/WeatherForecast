ce2c9a3ddb7277705e176072c954f094b3928def
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-08 16:58:46 -0400

    add locationlist component

---
+++ src/Components/LocationList/LocationList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import WeatherLocation from "../WeatherLocation/WeatherLocation";
+import PropTypes from "prop-types";
+import "./styles.css";
+
+const LocationList = ({ cities, onSelectedLocation }) => {
+  const handleWeatherLocationClick = city => {
+    console.log(city);
+    onSelectedLocation(city);
+  };
+
+  const citiesToComponent = cities => {
+    return cities.map(city => {
+      return (
+        <WeatherLocation
+          key={city}
+          city={city}
+          onWeatherLocationClick={() => handleWeatherLocationClick(city)}
+        />
+      );
+    });
+  };
+
+  return <div className="LocationList">{citiesToComponent(cities)}</div>;
+};
+
+LocationList.propTypes = {
+  cities: PropTypes.array,
+  onSelectedLocation: PropTypes.func
+};
+
+export default LocationList;
