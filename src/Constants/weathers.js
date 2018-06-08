2e8655fa59d40910bd9ab93da5a15f006611abda
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-08 17:00:07 -0400

    add thunder and drizzle constants

--- src/Constants/weathers.js
+++ src/Constants/weathers.js
@@ -4,3 +4,5 @@ export const SUN = "day-sunny";
 export const RAIN = "rain";
 export const SNOW = "snow";
 export const WINDY = "windy";
+export const THUNDER = "thunder";
+export const DRIZZLE = "drizzle";
