528ae7bf829617d0a10966bfafe108ccc6bb18d4
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-08 17:00:35 -0400

    import constants and complete getWeatherState

--- src/Services/transformWeather.js
+++ src/Services/transformWeather.js
@@ -1,8 +1,29 @@
 import convert from "convert-units";
-import { SUN } from "../Constants/weathers";
+import {
+  CLOUDY,
+  SUN,
+  RAIN,
+  SNOW,
+  THUNDER,
+  DRIZZLE
+} from "../Constants/weathers";
 
-const getWeatherState = weather_data => {
-  return SUN;
+const getWeatherState = weather => {
+  const { id } = weather[0];
+
+  if (id < 300) {
+    return THUNDER;
+  } else if (id < 400) {
+    return DRIZZLE;
+  } else if (id < 600) {
+    return RAIN;
+  } else if (id < 700) {
+    return SNOW;
+  } else if (id === 800) {
+    return SUN;
+  } else {
+    return CLOUDY;
+  }
 };
 
 const transformTemperature = kelvinTemp =>
@@ -16,13 +37,14 @@ const transformTemperature = kelvinTemp =>
 const transformWeather = weather_data => {
   const {
     main: { humidity, temp },
+    weather,
     wind: { speed }
   } = weather_data;
 
   return {
     humidity,
     temperature: transformTemperature(temp),
-    weatherState: getWeatherState(weather_data),
+    weatherState: getWeatherState(weather),
     wind: `${speed} m/s`
   };
 };
