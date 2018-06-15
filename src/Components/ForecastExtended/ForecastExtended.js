5d9ca9267aeef86a134d333286bca4e6a15759c0
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-15 10:34:37 -0400

    implement forecast extended

--- src/Components/ForecastExtended/ForecastExtended.js
+++ src/Components/ForecastExtended/ForecastExtended.js
@@ -3,19 +3,63 @@ import PropTypes from "prop-types";
 import Paper from "@material-ui/core/Paper";
 
 import "./styles.css";
+import { getForecast5 } from "../../Services/api";
+import { transformForecast } from "../../Services/transform";
+import ForecastItem from "../ForecastItem/ForecastItem";
 
 class ForecastExtended extends Component {
+  state = {
+    forecastData: null
+  };
+
+  renderForecastItemDays(forecastData) {
+    return forecastData.map(forecast => {
+      const { weekDay, hour, data } = forecast;
+      return (
+        <ForecastItem
+          key={`${weekDay}${hour}`}
+          weekDay={weekDay}
+          hour={hour}
+          data={data}
+        />
+      );
+    });
+  }
+  renderProgress() {
+    return <h3>Cargando pronóstico extendido...</h3>;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { city } = nextProps;
+
+    getForecast5(city)
+      .then(data => data.json())
+      .then(weather_data => {
+        const forecastData = transformForecast(weather_data);
+        this.setState({ forecastData });
+      })
+      .catch(err => {
+        console.log("error: ", err);
+      });
+  }
+
   render() {
     const { city } = this.props;
+    const { forecastData } = this.state;
 
     return (
-      <Paper elevation={4}>
+      <div>
         {
           <div className="detail">
             <h1>{!!city ? city : "No se ha seleccionado ciudad"}</h1>
+            {forecastData
+              ? this.renderForecastItemDays(forecastData)
+              : city
+                ? this.renderProgress()
+                : null}
           </div>
         }
-      </Paper>
+      </div>
     );
   }
 }
