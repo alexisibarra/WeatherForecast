a2dbcaf216cd8225e812e5972f43e6f47fcfdb57
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-22 17:51:54 -0400

    update to add combine reducers, and thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,15 @@
-import { createStore } from "redux";
-import { city } from "../reducers/city";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "../reducers/reducers";
+
 const initialState = {
-  city: "Buenos Aires, ar",
-  cities: [
-    "Buenos aires, ar",
-    "Caracas, ve",
-    "Bogota, co",
-    "Santiago, cl",
-    "Washington",
-    "Barcelona"
-  ]
+  city: "Buenos Aires, ar"
 };
 
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(city, initialState, composeEnhancers);
+export const store = createStore(
+  reducers,
+  initialState,
+  composeEnhancers(applyMiddleware(thunk))
+);
