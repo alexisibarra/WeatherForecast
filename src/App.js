b24b32fcf75f1c56d7a07f49c191e63ed5eb1c03
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-06-22 16:01:16 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -1,81 +1,38 @@
 import React, { Component } from "react";
 import { Grid, Col, Row } from "react-flexbox-grid";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
-import LocationList from "./Components/LocationList/LocationList";
-import ForecastExtended from "./Components/ForecastExtended/ForecastExtended";
 import "./styles.css";
+import LocationListContainer from "./containers/LocationListContainer";
+import ForecastExtendedContainer from "./containers/ForecastExtendedContainer";
 
-import { setCity } from "./actions/actions";
-
-const cities = [
-  "Buenos aires, ar",
-  "Caracas, ve",
-  "Bogota, co",
-  "Santiago, cl",
-  "Washington",
-  "Barcelona"
-];
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      city: ""
-    };
-  }
-
-  onSelectedLocation = city => {
-    this.setState({ city });
-
-    this.props.setCity(city);
-  };
-
-  render() {
-    const { city } = this.state;
-    return (
-      <Grid>
-        <Row>
-          <Col xs={12}>
-            <AppBar position="static" color="default" title="hola mundo">
-              <Toolbar>
-                <Typography variant="title" color="inherit">
-                  Title
-                </Typography>
-              </Toolbar>
-            </AppBar>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} md={6}>
-            <LocationList
-              cities={cities}
-              onSelectedLocation={this.onSelectedLocation}
-            />
-          </Col>
-          <Col xs={12} md={6}>
-            <ForecastExtended city={city} />
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
-}
-
-const mD = dispatch => ({
-  setCity: value => dispatch(setCity(value))
-});
-
-App.propTypes = {
-  setCity: PropTypes.func.isRequired
+const App = () => {
+  return (
+    <Grid>
+      <Row>
+        <Col xs={12}>
+          <AppBar position="static" color="default" title="hola mundo">
+            <Toolbar>
+              <Typography variant="title" color="inherit">
+                Title
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={6}>
+          <LocationListContainer />
+        </Col>
+        <Col xs={12} md={6}>
+          <ForecastExtendedContainer />
+        </Col>
+      </Row>
+    </Grid>
+  );
 };
 
-export default connect(
-  null,
-  mD
-)(App);
+export default App;
