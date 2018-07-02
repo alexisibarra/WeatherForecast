64ccd7b9377842745d06d849af4393a020029520
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-07-02 17:10:17 -0400

    implementing setweather action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,8 +1,14 @@
-import { getForecast5 } from "../Services/api";
-import { transformForecast } from "../Services/transform";
+import { getForecast5, getWeatherByCity } from "../Services/api";
+import { transformForecast, transformWeather } from "../Services/transform";
 
 export const SET_CITY = "SET_CITY";
 export const SET_FORECAST_DATA = "SET_FORECAST_DATA";
+export const SET_WEATHER = "SET_WEATHER";
+export const SET_WEATHER_CITY = "SET_WEATHER_CITY";
+export const GET_WEATHER_CITY = "GET_WEATHER_CITY";
+
+const getWeatherCity = payload => ({ type: GET_WEATHER_CITY, payload });
+const setWeatherCity = payload => ({ type: SET_WEATHER_CITY, payload });
 
 export const setCity = payload => ({ type: SET_CITY, payload });
 export const setForecastData = payload => ({
@@ -22,4 +28,21 @@ export const setSelectedCity = payload => dispatch => {
     .catch(err => {
       console.log("error: ", err);
     });
+};
+export const setWeather = payload => {
+  return dispatch => {
+    payload.map(city => {
+      dispatch(getWeatherCity(city));
+
+      return getWeatherByCity(city)
+        .then(data => data.json())
+        .then(weather_data => {
+          const weather = transformWeather(weather_data);
+          dispatch(setWeatherCity({ city, weather }));
+        })
+        .catch(err => {
+          console.log("error: ", err);
+        });
+    });
+  };
 };
