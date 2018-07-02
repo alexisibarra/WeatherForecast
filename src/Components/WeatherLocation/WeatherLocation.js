c2f056120ecb62582b493214f107597d963e82bc
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2018-07-02 17:11:13 -0400

    removing logic no longer needed and refactor as stateless functional component

--- src/Components/WeatherLocation/WeatherLocation.js
+++ src/Components/WeatherLocation/WeatherLocation.js
@@ -1,58 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { CircularProgress, Paper } from "@material-ui/core";
 
 import Location from "./Location/Location";
 import WeatherData from "./WeatherData/WeatherData";
-import { transformWeather } from "../../Services/transform";
 import "./styles.css";
-import { getWeatherByCity } from "../../Services/api";
 
-class WeatherLocation extends Component {
-  constructor({ city }) {
-    super();
-    this.state = {
-      city,
-      data: null
-    };
-  }
-
-  getWeatherInfo = () => {
-    const { city } = this.state;
-
-    getWeatherByCity(city)
-      .then(data => data.json())
-      .then(weather_data => {
-        const data = transformWeather(weather_data);
-        this.setState({ data });
-      })
-      .catch(err => {
-        console.log("error: ", err);
-      });
-  };
-
-  componentWillMount() {
-    this.getWeatherInfo();
-  }
-
-  render() {
-    const { onWeatherLocationClick } = this.props;
-    const { city, data } = this.state;
-
-    return (
-      <div className="WeatherLocationCont" onClick={onWeatherLocationClick}>
-        <Paper elevation={4}>
-          <Location location={city} />
-          {data ? <WeatherData data={data} /> : <CircularProgress size={50} />}
-        </Paper>
-      </div>
-    );
-  }
-}
+const WeatherLocation = ({ onWeatherLocationClick, city, data }) => (
+  <div className="WeatherLocationCont" onClick={onWeatherLocationClick}>
+    <Paper elevation={4}>
+      <Location location={city} />
+      {data ? <WeatherData data={data} /> : <CircularProgress size={50} />}
+    </Paper>
+  </div>
+);
 
 WeatherLocation.propTypes = {
   city: PropTypes.string,
-  onWeatherLocationClick: PropTypes.func
+  onWeatherLocationClick: PropTypes.func,
+  data: PropTypes.shape({
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired
+  })
 };
 
 export default WeatherLocation;
